f2bba7d5d3f3146a4e17992514205a762eccdcc3
Mohit Tomer <mstomer31@gmail.com>
2023-12-16 19:52:10 +0530

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Mohit Tomer on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
